d31b2d3f06a59899207dae5b484820030eb95882
Parth Mangla <parthmangla511@gmail.com>
2026-08-07 20:03:14 +0530

    Add new components and motivational footer to App
    Added new components for Card, Fame, and Gallery. Included a motivational footer section to encourage user engagement.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/navbar';
 import Hero from './components/hero';
 import Achievement from './components/achievement';
 import Timeline from './components/timeline';
+import Card from './components/card';
+import Fame from './components/fame';
+import Gallery from './components/gallery';
 
 function App() {
   return (
@@ -11,8 +14,18 @@ function App() {
       <Hero />
       <Achievement />
       <Timeline />
+      <Card />
+      <Fame />
+      <Gallery />
+      <footer>
+<div>
+End with a motivational section that encourages users to take action and make a positive impact in their lives.
+<br />
+[Join Geek Room]
+</div>
+</footer>
     </div>
-  )
+  );
 }
 
 export default App;
