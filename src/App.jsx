7cac0cd647e53229127dba6c991d1784f804d5e6
Parth Mangla <parthmangla511@gmail.com>
2026-08-16 21:13:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar';
-import Hero from './components/hero';
 import Achievement from './components/achievement';
 import Timeline from './components/timeline';
 import Card from './components/card';
 import Fame from './components/fame';
 import Gallery from './components/gallery';
 import Footer from './components/footer';
+import Monthly from "./components/monthly";
 import InteractiveBackground from './components/InteractiveBackground';
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
       <InteractiveBackground />
       <Navbar />
       <main>
-        <Hero />
         <Achievement />
+        <Monthly />
         <Timeline />
         <Card />
         <Fame />
